bd5bcb5b6e4ac54b82870e101a19cc6b03329792
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-24 11:27:22 +0100

    mobile app

--- lib/main.dart
+++ lib/main.dart
@@ -75,16 +75,19 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Container(
+      drawer: Container(color: Color.fromARGB(100, 8, 8, 8),
+        decoration: BoxDecoration(
+            border: Border(right: BorderSide(color: Color.fromARGB(255, 255, 255, 255)))
+        ),
         padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
-        width: 325, color: Color.fromARGB(100, 8, 8, 8),
+        width: 325, 
         child: Column(children: [
           Padding(padding: EdgeInsets.all(8), child: Icon(CupertinoIcons.camera_circle_fill, size: 120,color: Color.fromARGB(255, 150, 6, 6),),),
           Text("AI Camera", style:  TextStyle(fontWeight: FontWeight.bold, fontSize: 35),),
           Container(
             margin: EdgeInsets.fromLTRB(0, 25, 0, 25),
             decoration: BoxDecoration(
-            border: Border(top: BorderSide(color: Colors.grey))
+            border: Border(top: BorderSide(color: Color.fromARGB(255, 255, 255, 255)))
           ),)
         ],),
       ),
