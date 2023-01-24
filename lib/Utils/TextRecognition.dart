7d781290f35f5f4e29f6711325513e21f77576b8
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-25 00:36:24 +0100

    Mobile App

--- lib/Utils/TextRecognition.dart
+++ lib/Utils/TextRecognition.dart
@@ -1,29 +1,27 @@
-//import 'package:flutter/cupertino.dart';
-//import 'package:google_mlkit_commons/google_mlkit_commons.dart';
+import 'dart:ui';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
-//import 'art:ui';
 
 class TextRecognition {
 
   //  recognizing text
    static Future <String> captureText (InputImage inputImage) async {
-      // final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
-      // final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
-      // String text = recognizedText.text;
-      // for (TextBlock block in recognizedText.blocks) {
-      //     // final Rect rect = block.rect;
-      //     // final List<Offset> cornerPoints = block.cornerPoints;
-      //     final String text = block.text;
-      //     final List<String> languages = block.recognizedLanguages;
+      final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+      final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
+      String text = recognizedText.text;
+      for (TextBlock block in recognizedText.blocks) {
+          // final Rect rect_ = block.rect;
+          // final List<Offset> cornerPoints = block.cornerPoints;
+          final String text = block.text;
+          final List<String> languages = block.recognizedLanguages;
 
-      //     for (TextLine line in block.lines) {
-      //       // Same getters as TextBlock
-      //       for (TextElement element in line.elements) {
-      //         // Same getters as TextBlock
-      //       }
-      //     }
-      // }
-      return "text";
+          for (TextLine line in block.lines) {
+            // Same getters as TextBlock
+            for (TextElement element in line.elements) {
+              // Same getters as TextBlock
+            }
+          }
+      }
+      return "$text";
    }
 
 }
