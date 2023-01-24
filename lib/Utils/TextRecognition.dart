e606c09073e2238bf5a62158416f8a9814574030
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-24 23:59:08 +0100

    Mobile App

--- lib/Utils/TextRecognition.dart
+++ lib/Utils/TextRecognition.dart
@@ -1,29 +1,29 @@
 //import 'package:flutter/cupertino.dart';
-//import 'package:google_mlkit_commons/google_mlkit_commons.dart';
-import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:google_mlkit_commons/google_mlkit_commons.dart';
+//mport 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 //import 'dart:ui';
 
 class TextRecognition {
 
   //  recognizing text
    static Future <String> captureText (InputImage inputImage) async {
-      final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
-      final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
-      String text = recognizedText.text;
-      for (TextBlock block in recognizedText.blocks) {
-          // final Rect rect = block.rect;
-          // final List<Offset> cornerPoints = block.cornerPoints;
-          final String text = block.text;
-          final List<String> languages = block.recognizedLanguages;
+      // final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+      // final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
+      // String text = recognizedText.text;
+      // for (TextBlock block in recognizedText.blocks) {
+      //     // final Rect rect = block.rect;
+      //     // final List<Offset> cornerPoints = block.cornerPoints;
+      //     final String text = block.text;
+      //     final List<String> languages = block.recognizedLanguages;
 
-          for (TextLine line in block.lines) {
-            // Same getters as TextBlock
-            for (TextElement element in line.elements) {
-              // Same getters as TextBlock
-            }
-          }
-      }
-      return text;
+      //     for (TextLine line in block.lines) {
+      //       // Same getters as TextBlock
+      //       for (TextElement element in line.elements) {
+      //         // Same getters as TextBlock
+      //       }
+      //     }
+      // }
+      return "text";
    }
 
 }
