fa22bb458afd08a4a29b7c8acce0b31b6128847d
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-24 22:38:01 +0100

    Mobile App

--- lib/Utils/ImageProcessor.dart
+++ lib/Utils/ImageProcessor.dart
@@ -1,8 +1,9 @@
 import 'package:camera/camera.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_mlkit_commons/google_mlkit_commons.dart';
 
 class ImageProcessor {
-
+  
     static InputImage getInputImage (XFile file) {
       return InputImage.fromFilePath(file.path);
     }
