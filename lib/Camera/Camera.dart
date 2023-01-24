c95a808d92f6c915b941c9c5e81f16df74624ff1
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-24 10:48:47 +0100

    mobile app

--- lib/Camera/Camera.dart
+++ lib/Camera/Camera.dart
@@ -6,17 +6,25 @@ class Camera {
   static late List <CameraDescription> cameras;
 
   // available 
-  static Future <List <CameraDescription>> getAvailableCameras () async {
+  static Future <List <CameraDescription>> _getAvailableCameras () async {
     cameras = await availableCameras();
     return cameras;
   }
 
   static late CameraController controller;
   // camera controllers 
-  static Future <CameraController> getCameraController () async {
-    List <CameraDescription> desc = await getAvailableCameras();
-    controller = CameraController(desc[0], ResolutionPreset.max);
+  static Future <CameraController> getCameraController (int index) async {
+    List <CameraDescription> desc = await _getAvailableCameras();
+    controller = CameraController(desc[index], ResolutionPreset.max);
     return controller;
+  }
+
+  // initializing the camera controller
+  static Future <void> initCamera (CameraController camController, Function callback) async {
+    try {
+     await camController.initialize();
+     callback(false, "Camera successfully initialized");
+    } catch (exception) { callback(false, exception); }
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
+import 'package:cam_ai/Camera/Camera.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Color.fromARGB(255, 223, 222, 222), // transparent status bar
+  ));
+  runApp(const MyApp()); initCamera();
+}
+
+late CameraController camController;
+
+void initCamera () async {
+  camController = await Camera.getCameraController(0);
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -29,21 +40,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    camController.dispose();
+  }
+
+  void initCam () async {
+    camController = await Camera.getCameraController(0);
+    await Camera.initCamera(camController, (bool status, value){
+    print(value);
+  });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    initCam();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Container(
+        padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+        width: 325, color: Colors.white,
+        child: Column(children: [
+          Padding(padding: EdgeInsets.all(8), child: Icon(CupertinoIcons.camera_circle_fill, size: 120,color: Color.fromARGB(255, 150, 6, 6),),),
+          Text("AI Camera", style:  TextStyle(fontWeight: FontWeight.bold, fontSize: 35),),
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 25, 0, 25),
+            decoration: BoxDecoration(
+            border: Border(top: BorderSide(color: Colors.grey))
+          ),)
+        ],),
+      ),
       body: Container(
         child: Stack(children: [
-          
+          CameraPreview(camController)
         ],),
         color: Colors.transparent,
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        backgroundColor: Color.fromARGB(255, 150, 6, 6),
+        onPressed: (){
+       
+        },
         tooltip: 'Camera',
-        child: const Icon(CupertinoIcons.camera_circle_fill),
+        child: const Icon(CupertinoIcons.camera_fill),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
