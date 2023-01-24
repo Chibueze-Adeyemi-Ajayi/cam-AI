94f4169cdc2138ea10d14ef4c6935fc7d4579795
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-24 23:02:23 +0100

    Mobile App

--- lib/Utils/TextRecognition.dart
+++ lib/Utils/TextRecognition.dart
@@ -10,6 +10,19 @@ class TextRecognition {
       final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
       final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
       String text = recognizedText.text;
+      for (TextBlock block in recognizedText.blocks) {
+          // final Rect rect = block.rect;
+          // final List<Offset> cornerPoints = block.cornerPoints;
+          final String text = block.text;
+          final List<String> languages = block.recognizedLanguages;
+
+          for (TextLine line in block.lines) {
+            // Same getters as TextBlock
+            for (TextElement element in line.elements) {
+              // Same getters as TextBlock
+            }
+          }
+      }
       return text;
    }
 
