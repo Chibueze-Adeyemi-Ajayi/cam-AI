aec44292512231fead4db1a4c3d1bf29481c64aa
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-25 02:02:32 +0100

    Mobile App\

--- lib/Utils/InkRecognition.dart
+++ lib/Utils/InkRecognition.dart
@@ -3,12 +3,19 @@ import 'package:learning_digital_ink_recognition/learning_digital_ink_recognitio
 class InkRecognition {
 
   // language code
-  static String languageCode = "en"; // BCP-47 Code from https://developers.google.com/ml-kit/vision/digital-ink-recognition/base-models?hl=en#text
-  static DigitalInkRecognition recognition = DigitalInkRecognition(model: 'en-US');
+  static String model = "en-US"; // BCP-47 Code from https://developers.google.com/ml-kit/vision/digital-ink-recognition/base-models?hl=en#text
+  static DigitalInkRecognition recognition = DigitalInkRecognition(model: model);
 
   // processing ink
   static Future processInk () async {
-    
+    // downloading model 
+    // model is a string value, e.g. 'en-US'
+    bool isDownloaded = await DigitalInkModelManager.isDownloaded(model);
+    // download the model first if it's not downloaded yet
+    if (!isDownloaded) {
+      await DigitalInkModelManager.download(model);
+    }
+
   }
 
 }
