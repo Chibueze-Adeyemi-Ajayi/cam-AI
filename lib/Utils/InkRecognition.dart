cfbea7a0653b6be0932c5627e077d8d0c58fb21c
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-25 01:59:37 +0100

    Mobile App\

--- lib/Utils/InkRecognition.dart
+++ lib/Utils/InkRecognition.dart
@@ -1,7 +1,14 @@
 import 'package:google_mlkit_digital_ink_recognition/google_mlkit_digital_ink_recognition.dart';
 
 class InkRecognition {
+
   // language code
-static String languageCode = "en"; // BCP-47 Code from https://developers.google.com/ml-kit/vision/digital-ink-recognition/base-models?hl=en#text
-final digitalInkRecognizer = DigitalInkRecognizer(languageCode: languageCode);
+  static String languageCode = "en"; // BCP-47 Code from https://developers.google.com/ml-kit/vision/digital-ink-recognition/base-models?hl=en#text
+  static DigitalInkRecognition recognition = DigitalInkRecognition(model: 'en-US');
+
+  // processing ink
+  static Future processInk () async {
+    
+  }
+
 }
