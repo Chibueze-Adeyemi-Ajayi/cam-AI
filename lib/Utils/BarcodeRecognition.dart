f1f6f31124f0becbe433536b8c1b74d5299c6cb4
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-30 11:46:27 +0100

    CAM AI Mobile App

--- lib/Utils/BarcodeRecognition.dart
+++ lib/Utils/BarcodeRecognition.dart
@@ -1,5 +1,13 @@
+import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
+
 class BarcodeRecognition {
 
-  
+  final List<BarcodeFormat> formats = [BarcodeFormat.all];
+  static var barcodeScanner;
+
+  BarcodeRecognition getInstance () {
+    barcodeScanner = BarcodeScanner(formats: formats);
+    return new BarcodeRecognition();
+  }
 
 }
