ffb01b2e0d4a6692f977bfc68c54f18827c68ef9
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-31 08:17:48 +0100

    Mobile App

--- lib/main.dart
+++ lib/main.dart
@@ -113,7 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ));
   }
-// this is commi
+// this is commit
   @override
   Widget build(BuildContext context) {
     return Scaffold(
