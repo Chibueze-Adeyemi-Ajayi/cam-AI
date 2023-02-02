401c38fcd9dd19cf925a1690827197c0096ec1b5
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-02 23:19:50 +0100

    Mobile Application

--- lib/Utils/TextRecognition.dart
+++ lib/Utils/TextRecognition.dart
@@ -3,7 +3,7 @@ import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart
 
 class TextRecognition {
 
-  //  recognizing text from ml\
+  //  recognizing text from m
    static Future <String> captureText (InputImage inputImage) async {
       print("object");
       final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
