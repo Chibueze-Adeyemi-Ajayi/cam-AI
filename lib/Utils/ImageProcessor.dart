ffa65c643ccfa4658ad808304eea776b9f45dfe5
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-02 23:01:59 +0100

    Mobile Application

--- lib/Utils/ImageProcessor.dart
+++ lib/Utils/ImageProcessor.dart
@@ -2,7 +2,7 @@ import 'package:camera/camera.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
-// the image processing cod
+// the image processing co
 class ImageProcessor {
   
     static InputImage getInputImage (XFile file) {
