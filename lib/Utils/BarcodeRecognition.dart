e8454547a8a2ad32df22107a33493d5c465902aa
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-02 22:25:04 +0100

    Mobile Application

--- lib/Utils/BarcodeRecognition.dart
+++ lib/Utils/BarcodeRecognition.dart
@@ -1,6 +1,6 @@
 import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
 import "dart:ui";
-// recognizing 
+// recognizing b
 class BarcodeRecognition {
 
   Future processCode (InputImage image) async {
