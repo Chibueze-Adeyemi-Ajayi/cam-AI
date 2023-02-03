ab86fc15f89eabd91ba21c458cfcab22af1d0845
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-03 17:04:31 +0100

    Mobile App

--- lib/Utils/InkRecognition.dart
+++ lib/Utils/InkRecognition.dart
@@ -1,5 +1,5 @@
 import 'package:learning_digital_ink_recognition/learning_digital_ink_recognition.dart';
-// this code section is for ink recogni
+// this code section is for ink recogn
 class InkRecognition {
 
   // language code
