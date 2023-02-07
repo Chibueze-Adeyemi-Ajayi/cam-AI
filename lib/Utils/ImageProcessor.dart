f87c651dab75b2e8d713c638be7f48d1ff02b991
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-07 02:51:44 +0100

    mobile app

--- lib/Utils/ImageProcessor.dart
+++ lib/Utils/ImageProcessor.dart
@@ -8,50 +8,12 @@ class ImageProcessor {
       return InputImage.fromFilePath(file.path);
     }
 
-    static image_lib.Image convertYUV420ToImage(CameraImage cameraImage) {
-    final width = cameraImage.width;
-    final height = cameraImage.height;
-
-    final yRowStride = cameraImage.planes[0].bytesPerRow;
-    final uvRowStride = cameraImage.planes[1].bytesPerRow;
-    final uvPixelStride = cameraImage.planes[1].bytesPerPixel!;
-
-    final image = image_lib.Image(height: height, width: width);
-
-    for (var w = 0; w < width; w++) {
-      for (var h = 0; h < height; h++) {
-        final uvIndex =
-            uvPixelStride * (w / 2).floor() + uvRowStride * (h / 2).floor();
-        final index = h * width + w;
-        final yIndex = h * yRowStride + w;
-
-        final y = cameraImage.planes[0].bytes[yIndex];
-        final u = cameraImage.planes[1].bytes[uvIndex];
-        final v = cameraImage.planes[2].bytes[uvIndex];
-
-        final image_lib.ImageData? img_data = image.data;
-        img_data[index] = (yuv2rgb(y, u, v));
-        
-      }
+    static inputImage convertCameraImage (CameraImage cam_img) {
+      image_lib.Image img = image_lib.Image.fromBytes(
+          width: cam_img.width, 
+          height: cam_img.height, 
+          bytes: cam_img.planes[0].bytes,
+          format: image_lib.Format.bgra);
     }
-    return image;
-  }
-
-  static int yuv2rgb(int y, int u, int v) {
-    // Convert yuv pixel to rgb
-    var r = (y + v * 1436 / 1024 - 179).round();
-    var g = (y - u * 46549 / 131072 + 44 - v * 93604 / 131072 + 91).round();
-    var b = (y + u * 1814 / 1024 - 227).round();
-
-    // Clipping RGB values to be inside boundaries [ 0 , 255 ]
-    r = r.clamp(0, 255);
-    g = g.clamp(0, 255);
-    b = b.clamp(0, 255);
-
-    return 0xff000000 |
-        ((b << 16) & 0xff0000) |
-        ((g << 8) & 0xff00) |
-        (r & 0xff);
-  }
 
 }
