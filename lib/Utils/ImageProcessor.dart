af4647b8e771e3566fb002778846c378190ce017
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-07 03:09:23 +0100

    mobile app

--- lib/Utils/ImageProcessor.dart
+++ lib/Utils/ImageProcessor.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:camera/camera.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image/image.dart' as image_lib;
@@ -8,12 +10,38 @@ class ImageProcessor {
       return InputImage.fromFilePath(file.path);
     }
 
-    static inputImage convertCameraImage (CameraImage cam_img) {
-      image_lib.Image img = image_lib.Image.fromBytes(
-          width: cam_img.width, 
-          height: cam_img.height, 
-          bytes: cam_img.planes[0].bytes,
-          format: image_lib.Format.bgra);
+    static InputImage convertCameraImage (CameraImage cam_img) {
+      final camera; // your camera instance
+      final WriteBuffer allBytes = WriteBuffer();
+      for (Plane plane in cameraImage.planes) {
+        allBytes.putUint8List(plane.bytes);
+      }
+      final bytes = allBytes.done().buffer.asUint8List();
+
+      final Size imageSize = Size(cameraImage.width.toDouble(), cameraImage.height.toDouble());
+
+      InputImageRotation imageRotation = InputImageRotation.Rotation_0deg;
+      switch (camera.sensorOrientation) {
+        case 0:
+          imageRotation = InputImageRotation.rotation0deg;
+          break;
+        case 90:
+          imageRotation = InputImageRotation.rotation90deg;
+          break;
+        case 180:
+          imageRotation = InputImageRotation.rotation180deg;
+          break;
+        case 270:
+          imageRotation = InputImageRotation.rotation270deg;
+          break;
+      }
+
+      final inputImageData = InputImageData(
+        size: imageSize,
+        imageRotation: imageRotation,
+      );
+
+      final inputImage = InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
     }
 
 }
