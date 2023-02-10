100e463d0df67175b2c8dc959b91134f669a1e31
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-10 22:16:24 +0100

    mobile App

--- lib/Utils/mycanvas.dart
+++ lib/Utils/mycanvas.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
-// this is t
+// this is th
 class OpenPainter extends CustomPainter {
 @override
   void paint(Canvas canvas, Size size) {
