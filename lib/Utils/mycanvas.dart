e1dfb144e60258c462f5a377fc2d0abb8028169e
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-10 22:29:11 +0100

    mobile App

--- lib/Utils/mycanvas.dart
+++ lib/Utils/mycanvas.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
-// this is this the paint application for displaying detected 
+// this is this the paint application for displaying detected p
 class OpenPainter extends CustomPainter {
 @override
   void paint(Canvas canvas, Size size) {
