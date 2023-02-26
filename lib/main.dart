1534fc1c5fc759ea3c24a474f10422405b8256c9
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-26 17:51:25 +0100

    mobile

--- lib/main.dart
+++ lib/main.dart
@@ -61,6 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 // this function initializes the camera on the mobile devices
   void initCam () async {
+    // 
     camController = CameraController(
       cameras[0], ResolutionPreset.low
     );
