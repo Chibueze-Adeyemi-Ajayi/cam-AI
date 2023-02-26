57a19ea20a6bf75b26874636495eb2d9e433fa29
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-26 17:44:49 +0100

    mobile

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
     camController.dispose();
   }
-// this functi
+// this functio
   void initCam () async {
     camController = CameraController(
       cameras[0], ResolutionPreset.low
