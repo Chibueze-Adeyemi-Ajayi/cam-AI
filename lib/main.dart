17764fdfb558d90dd9354ca947878946ea79e96f
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-27 19:48:26 +0100

    mobile

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void captureVideo () {
     camController.startVideoRecording();
   }
-// This disposes the camera con
+// This disposes the camera cont
   @override
   void dispose() {
     // TODO: implement dispose 
