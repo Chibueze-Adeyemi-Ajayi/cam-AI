6c3a97aa66080186b6e5d6627d8b0e71a9b0981e
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-28 20:44:26 +0100

    mobile app

--- lib/Utils/PoseDetection.dart
+++ lib/Utils/PoseDetection.dart
@@ -5,7 +5,7 @@ import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
 // Pose detection class 
 
 class PoseDetection {
-  // connecting to google's algorith
+  // connecting to google's algorithm
   static final options = PoseDetectorOptions();
   static final poseDetector = PoseDetector(options: options);
   static Future <List<Pose>> getPose(inputImage) async {
