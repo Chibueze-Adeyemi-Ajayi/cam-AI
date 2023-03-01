651cb1d23c02cb8fba180ff201fffb369daf4051
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-01 12:05:44 +0100

    Mobile App

--- lib/Utils/PoseDetection.dart
+++ lib/Utils/PoseDetection.dart
@@ -12,7 +12,7 @@ class PoseDetection {
     final List <Pose> poses = await poseDetector.processImage(inputImage);
     return poses;
   }
-  // closing connection to pose det
+  // closing connection to pose dete
   static close () {
     poseDetector.close();
   }
