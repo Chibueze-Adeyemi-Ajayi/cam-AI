2a304acb51482e150e53b1a0264e51f27842f3a2
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-30 22:14:22 +0100

    Mobile App

--- lib/Utils/PoseDetection.dart
+++ lib/Utils/PoseDetection.dart
@@ -3,7 +3,7 @@
 import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
 
 // Pose detection class 
-// Go
+// Goo
 class PoseDetection {
   // connecting to google's algorithm
   static final options = PoseDetectorOptions();
