2a4a22e2861e7dd3ae8ddbda230d2cff052eb3a4
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-30 22:10:54 +0100

    Mobile App

--- lib/Utils/InkRecognition.dart
+++ lib/Utils/InkRecognition.dart
@@ -1,6 +1,6 @@
 import 'package:learning_digital_ink_recognition/learning_digital_ink_recognition.dart';
 
-// digital ink recognition algorithm from google ML k
+// digital ink recognition algorithm from google ML ki
 class InkRecognition {
 
   // language code
