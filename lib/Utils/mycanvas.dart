54a46a45323f2fb8d1f2591d54cf3b3b765a0d70
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-31 21:02:51 +0100

    Mobile App

--- lib/Utils/mycanvas.dart
+++ lib/Utils/mycanvas.dart
@@ -14,7 +14,7 @@ final List<Offset> offsets;
      var paint2 = Paint()
       ..color = Color.fromARGB(255, 255, 39, 1)
       ..strokeWidth = 1;
-      // drawing p
+      // drawing po
       canvas.drawPoints(PointMode.points, offsets, paint1);
    
     // linking the shoulders together
