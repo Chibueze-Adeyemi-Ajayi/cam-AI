16d37287acf80bec317feba945a3ab83c5e9b824
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-01 22:22:02 +0100

    Mobile App

--- lib/Utils/ImageProcessor.dart
+++ lib/Utils/ImageProcessor.dart
@@ -14,7 +14,7 @@ class ImageProcessor {
       return InputImage.fromFilePath(file.path);
     }
 
-    // c
+    // ca
     static InputImage getInputImageFromLiveStream (CameraImage cameraImage, camera) {
       final WriteBuffer allBytes = WriteBuffer();
       for (Plane plane in cameraImage.planes) {
