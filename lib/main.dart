38d28d21ca8ad7dc2d84af52a497c1ccbe69b4b6
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-03 23:20:42 +0100

    Mobile App

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // mobile app for:
 // 1. recognize texts
-// 2. scanning bar 
+// 2. scanning bar
 
 import 'package:cam_ai/Utils/BarcodeRecognition.dart';
 import 'package:cam_ai/Utils/ImageProcessor.dart';
