c52dc60c5ec220a3aa645b20fa89c0c685642b1a
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-04 22:30:39 +0100

    Mobile App

--- lib/main.dart
+++ lib/main.dart
@@ -157,7 +157,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-  // pose detection paramet
+  // pose detection paramete
   List <String> landmarks = [ 
     "Right Shoulder", "Left Shoulder", "Right Elbow", "Left Elbow", 
     "Right Wrist", "Left Wrist", "Right Hip", "Left Hip", 
