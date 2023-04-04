91f833904a93027ca722ae14034bf06493540da0
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-04 22:26:02 +0100

    Mobile App

--- lib/main.dart
+++ lib/main.dart
@@ -157,7 +157,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
- 
+  
   List <String> landmarks = [ 
     "Right Shoulder", "Left Shoulder", "Right Elbow", "Left Elbow", 
     "Right Wrist", "Left Wrist", "Right Hip", "Left Hip", 
