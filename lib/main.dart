2e73c0a6f96330a29204315ce5db99e417234295
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-05 21:32:22 +0100

    Mobile App

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ late CameraController camController;
 late Future<void> cameraValue;
 late List <CameraDescription> cameras;
 
-//
+/
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
