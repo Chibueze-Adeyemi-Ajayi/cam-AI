d83fb05c0470949090005d2ad6c1286e2df9da3a
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-10 22:13:39 +0100

    mobile App

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
 
 late CameraController camController; // camera controller 
 late Future<void> cameraValue; // future loader 
-late List <CameraDescription> cameras; // camera descri
+late List <CameraDescription> cameras; // camera descrip
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
