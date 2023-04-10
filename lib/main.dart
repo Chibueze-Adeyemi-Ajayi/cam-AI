b652a8c841b354617cbb0556e3321a5d3f021204
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-10 22:06:02 +0100

    mobile App

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
 
-late CameraController camController; // came
+late CameraController camController; // cametr
 late Future<void> cameraValue;
 late List <CameraDescription> cameras;
 
