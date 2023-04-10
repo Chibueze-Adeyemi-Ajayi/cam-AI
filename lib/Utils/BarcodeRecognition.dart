40b76f11c07e4137102236a090f163a3bc2289c5
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-10 22:18:14 +0100

    mobile App

--- lib/Utils/BarcodeRecognition.dart
+++ lib/Utils/BarcodeRecognition.dart
@@ -7,7 +7,7 @@ class BarcodeRecognition {
 
     try {
 
-      String? response = "Empty barcode"; // default barcode responseb
+      String? response = "Empty barcode"; // default barcode response
       final List<BarcodeFormat> formats = [BarcodeFormat.all];
       var barcodeScanner;
 
