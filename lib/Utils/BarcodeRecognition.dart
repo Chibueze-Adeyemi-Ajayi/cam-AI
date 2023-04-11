6911d5567211e314018ac764c23e4230696830bc
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-11 21:00:15 +0100

    mobile App

--- lib/Utils/BarcodeRecognition.dart
+++ lib/Utils/BarcodeRecognition.dart
@@ -14,7 +14,7 @@ class BarcodeRecognition {
       barcodeScanner = BarcodeScanner(formats: formats);
       final List<Barcode> barcodes = await barcodeScanner.processImage(image);
       //response = barcodes.toString();
-        for (Barcode barcode in barcodes) { // foreach loop
+        for (Barcode barcode in barcodes) { // foreach loop 
               final BarcodeType type = barcode.type;
               final Rect? boundingBox = barcode.boundingBox;
               final String? displayValue = barcode.displayValue;
