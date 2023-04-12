612748d802e46e7e22deb6e3260fc6d8a510c77f
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-12 21:53:32 +0100

    mobile App

--- lib/Utils/ImageProcessor.dart
+++ lib/Utils/ImageProcessor.dart
@@ -33,7 +33,7 @@ class ImageProcessor {
           imageRotation = InputImageRotation.rotation90deg; // 90 degrees
           break;
         case 180:
-          imageRotation = InputImageRotation.rotation180deg; // 180 de
+          imageRotation = InputImageRotation.rotation180deg; // 180 deg
           break;
         case 270:
           imageRotation = InputImageRotation.rotation270deg;
