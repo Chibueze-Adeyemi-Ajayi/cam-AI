8cbf08282c4d5d2cbe96ccf8f486d18acd237063
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-12 21:45:50 +0100

    mobile App

--- lib/Utils/ImageProcessor.dart
+++ lib/Utils/ImageProcessor.dart
@@ -11,7 +11,7 @@ class ImageProcessor {
   
     // static image
     static InputImage getInputImage (var file) {
-      return InputImage.fromFilePath(file.path); // image file pa
+      return InputImage.fromFilePath(file.path); // image file pat
     }
 
     // camera
