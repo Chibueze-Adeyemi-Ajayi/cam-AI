359c9371917709d2a55a531bb1a8b2cb297e5684
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-13 21:45:04 +0100

    Mobile App

--- lib/Utils/ImageProcessor.dart
+++ lib/Utils/ImageProcessor.dart
@@ -43,7 +43,7 @@ class ImageProcessor {
       final inputImageData = InputImageData( // image data
         size: imageSize, // image size
         planeData: null,
-        imageRotation: imageRotation, /
+        imageRotation: imageRotation, //
         inputImageFormat: InputImageFormat.bgra8888
       ); // image data 
 
